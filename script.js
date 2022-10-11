f44d38814a19da1bdcc0f11cd80fb3a440a00e3d
wser <w.web.dev@gmail.com>
2022-10-11 15:19:45 +0200

    custom local storrage atribute

--- script.js
+++ script.js
@@ -227,64 +227,98 @@ function runIt() {
   connectDivs('#box1', '#box4', 'green', 0.3);
 }
 
-var dataSet = load('w_Edata');
+/*** ORIGINAL
+ */
 
-dataSet = [];
-// dataSet.push({
-//   name: 'Dan Cruickshank',
-//   company: 'Fishtank',
-// });
-
-//save('w_Edata', dataSet);
-//console.log(dataSet);
-
-/* MAKE TABLE Excle */
-for (let i = 0; i < 6; i++) {
-  let row = $('#table2').insertRow(-1);
-  for (let j = 0; j < 6; j++) {
-    let letter = String.fromCharCode('A'.charCodeAt(0) + j - 1);
-    /* prettier-ignore */
-    row.insertCell(-1).innerHTML = i && j ? "<input id='" + letter + i + "'/>" : i || letter;
+for (var i = 0; i < 3; i++) {
+  var row = $('#table2').insertRow(-1);
+  for (var j = 0; j < 3; j++) {
+    var letter = String.fromCharCode('A'.charCodeAt(0) + j - 1);
+    row.insertCell(-1).innerHTML =
+      i && j ? "<input id='" + letter + i + "'/>" : i || letter;
   }
 }
-/** EXCEL enabler */
 /* prettier-ignore */
-let DATA = {}, INPUTS = [...$$('input')], arr = [];
-INPUTS.forEach((elm) => {
-  /* prettier-ignore */
-  elm.onfocus = (e) => { e.target.value = DATA[elm.id] || ''; };
-  // elm.onfocus = (e) => { e.target.value = localStorage[e.target.id] || ''; };
-  /* prettier-ignore */
-  elm.onblur = (e) => { save('w_Edata', arr); computeAll(); };
-  //elm.onblur = (e) => { localStorage[e.target.id] = e.target.value; computeAll(); };
-  let getter = function () {
-    //arr = load('w_Edata');
-    let value = DATA[elm.id] || '';
-    if (value.charAt(0) == '=') with (DATA) return eval(value.substring(1));
-    else {
-      return isNaN(parseFloat(value)) ? value : parseFloat(value);
-    }
+let DATA = {}, INPUTS = [...$$('input')], oneMore;
+
+const obj = {};
+const lsObj = load('w_Excel');
+
+let computeAll = () =>
+  INPUTS.forEach((elm, i) => {
+    try {
+      elm.value = DATA[elm.id];
+
+      //oneMore = { id: elm.id, value: elm.value };
+      //obj[i] = { ...oneMore }; // fill the object with data from inputs
+    } catch (e) {}
+  });
+
+// /* prettier-ignore */
+// let items = load('w_Excel'), arr = [], arr2 = [];
+// let item = Object.entries(items).forEach(([key, value]) => {
+//   arr.push(value);
+//   arr2.push(arr[key].id);
+// });
+// console.log(arr);
+
+/** EXCEL enabler */
+INPUTS.forEach((elm, i) => {
+  elm.onfocus = (e) => {
+    //console.log(lsObj);
+    e.target.value = localStorage[e.target.id] || ''; // write value inside cell/input
   };
+  /* prettier-ignore */
+  elm.onblur = (e) => { 
+    //localStorage[e.target.id] = e.target.value;    
+    oneMore = { id: e.target.id, value: e.target.value };
+    obj[i] = { ...oneMore }; // fill the object with data from inputs   
+    //computeAll(); // spin the obj, add data
+    save('w_Excel', obj);// save value to local storage
+  };
+  let getter = () => {
+    // let value = localStorage[elm.id] || '';
+    eval(lsObj[elm.id].substring(1)) || '';
+    //let value = elm.value || '';
+    // if (value.charAt(0) == '=') with (DATA) return eval(value.substring(1));
+    // else return isNaN(parseFloat(value)) ? value : parseFloat(value);
+  };
+
   Object.defineProperty(DATA, elm.id, { get: getter });
   Object.defineProperty(DATA, elm.id.toLowerCase(), { get: getter });
-
-  //local.set('w_Excel', arr);
 });
-console.log(arr);
 
-/* prettier-ignore */ /* writes values to array */
-let computeAll = () => INPUTS.forEach( (elm) => { 
-  try { 
-    elm.value = DATA[elm.id]; 
-    let oneMore = { id: elm.id, value: elm.value };
-    var index = arr.findIndex((x) => x.id == elm.id);
-    index === -1 ? arr.push(oneMore) : console.log('object already exists');
-    arr.push(oneMore);    
-      
-  } catch (e) {} 
-});
-save('w_Edata', arr);
 computeAll();
+
+/*** ORIGINAL
+ */
+
+//  for (var i = 0; i < 6; i++) {
+//   var row = $('#table2').insertRow(-1);
+//   for (var j = 0; j < 6; j++) {
+//     var letter = String.fromCharCode('A'.charCodeAt(0) + j - 1);
+//     row.insertCell(-1).innerHTML =
+//       i && j ? "<input id='" + letter + i + "'/>" : i || letter;
+//   }
+// }
+// /** EXCEL enabler */
+// /* prettier-ignore */
+// let DATA = {}, INPUTS = [].slice.call($$('input'));
+// INPUTS.forEach((elm) => {
+//   elm.onfocus = (e) => (e.target.value = localStorage[e.target.id] || '');
+//   /* prettier-ignore */
+//   elm.onblur = (e) => { localStorage[e.target.id] = e.target.value; computeAll(); };
+//   let getter = function () {
+//     let value = localStorage[elm.id] || '';
+//     if (value.charAt(0) == '=') with (DATA) return eval(value.substring(1));
+//     else return isNaN(parseFloat(value)) ? value : parseFloat(value);
+//   };
+//   Object.defineProperty(DATA, elm.id, { get: getter });
+//   Object.defineProperty(DATA, elm.id.toLowerCase(), { get: getter });
+// });
+// /* prettier-ignore */
+// let computeAll = () => INPUTS.forEach( (elm) => { try {elm.value = DATA[elm.id];} catch (e) {} });
+// computeAll();
 
 /** TABLE enabler */
 // (A) GET HTML TABLE
